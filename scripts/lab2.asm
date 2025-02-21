8870ed4dd9effe223ce54f43306263d521be1c5a
Hunk <mougenotflorent@gmail.com>
2025-02-21 16:58:06 +0100

    feat(#71): changed fossil acquisition method

--- scripts/lab2.asm
+++ scripts/lab2.asm
@@ -7,7 +7,120 @@ Lab2TextPointers:
 	dw Lab2Text3
 
 Lab2Text1:
-	TX_FAR _Lab2Text1
+	TX_ASM
+	CheckBothEventsSet EVENT_GOT_DOME_FOSSIL, EVENT_GOT_HELIX_FOSSIL
+	jr z, .showAlreadyBoughtText;already bought fossils
+	CheckEvent EVENT_908	;has elite 4 been beaten?
+	jr nz, .recognizeChampion
+	ld hl, Lab2TextFossile
+	call PrintText
+	jp .textEnd
+.showAlreadyBoughtText
+	ld hl, Lab2TextFossile2
+	call PrintText
+	jp .textEnd
+.recognizeChampion
+	ld hl, Lab2TextFossileChampion
+	call PrintText
+	call ManualTextScroll
+	CheckEvent EVENT_GOT_DOME_FOSSIL
+	;if player choose dome ask for helix
+	jr nz, .displayHelixText
+	;otherwise ask for dome
+	ld hl, Lab2TextFossileDome
+	call PrintText
+	call ManualTextScroll
+	jr .askForBuyingFossile
+.displayHelixText
+	ld hl, Lab2TextFossileHelix
+	call PrintText
+	jr .askForBuyingFossile
+.askForBuyingFossile
+	;prompt a yes/no choice
+	call YesNoChoice
+	;load the player choice
+	ld a, [wCurrentMenuItem]
+	;check the player choice
+	and a
+	jr nz, .textNo	;if no, jump
+	;otherwise charge 10000 money
+	ld [hMoney], a	
+	ld [hMoney + 2], a
+	ld a, $A0
+	ld [hMoney + 1], a
+	call HasEnoughMoney
+	jr nc, .enoughMoney
+	; not enough money
+	ld hl, MoveRelearnerNotEnoughMoneyText
+	ld hl, Lab2TextFossileNoMoney
+	call PrintText
+	jr .textEnd
+.textNo
+	ld hl, Lab2TextFossileNo
+	call PrintText
+	jr .textEnd
+.enoughMoney
+	CheckEvent EVENT_GOT_DOME_FOSSIL
+	;if player choose dome give helix
+	jr nz, .giveHelix
+	;otherwise give dome
+	lb bc, DOME_FOSSIL, 1
+	call GiveItem
+	jr nc, .bagFull
+	SetEvent EVENT_GOT_DOME_FOSSIL
+	jr .takeMoneyAndThanks
+.giveHelix
+	lb bc, HELIX_FOSSIL, 1
+	call GiveItem
+	jr nc, .bagFull
+	SetEvent EVENT_GOT_HELIX_FOSSIL
+.takeMoneyAndThanks
+	; Charge 10000 money
+	xor a
+	ld [wPriceTemp], a
+	ld [wPriceTemp + 2], a	
+	ld a, $A0
+	ld [wPriceTemp + 1], a	
+	ld hl, wPriceTemp + 2
+	ld de, wPlayerMoney + 2
+	ld c, $3
+	predef SubBCDPredef
+	ld hl, Lab2TextFossileYes
+	call PrintText
+	jr .textEnd
+.bagFull
+	ld hl, Lab2TextFossileBagFull
+	call PrintText
+	jr .textEnd
+.textEnd
+	jp TextScriptEnd
+
+Lab2TextFossile:
+	TX_FAR _Lab2TextFossile
+	db "@"
+Lab2TextFossile2:
+	TX_FAR _Lab2TextFossile2
+	db "@"
+Lab2TextFossileChampion:
+	TX_FAR _Lab2TextFossileChampion
+	db "@"
+Lab2TextFossileDome:
+	TX_FAR _Lab2TextFossileDome
+	db "@"
+Lab2TextFossileHelix:
+	TX_FAR _Lab2TextFossileHelix
+	db "@"
+Lab2TextFossileYes:
+	TX_FAR _Lab2TextFossileYes
+	db "@"
+Lab2TextFossileNo:
+	TX_FAR _Lab2TextFossileNo
+	db "@"
+Lab2TextFossileNoMoney:
+	TX_FAR _Lab2TextFossileNoMoney
+	db "@"
+Lab2TextFossileBagFull:
+	TX_FAR _Lab2TextFossileBagFull
 	db "@"
 
 Lab2Text2:
--- text/maps/cinnabar_lab_trade_room.asm
+++ text/maps/cinnabar_lab_trade_room.asm
@@ -1,4 +1,4 @@
-_Lab2Text1::
+_Lab2TextFossile::
 	text "J'ai trouvé cet"
 	line "étrange fossile"
 	cont "sur le Mont"
@@ -7,4 +7,76 @@ _Lab2Text1::
 	para "On dirait un"
 	line "#mon"
 	cont "préhistorique!"
+	done
+
+_Lab2TextFossile2::
+	text "Bon j'ai pas mal"
+	line "de choses à finir"
+	cont "avant de partir."
+	done
+
+_Lab2TextFossileChampion::
+	text "Hé mais tu ne"
+	line "serais pas le"
+	cont "nouveau champion?"
+
+	para "J'ai toujours rê-"
+	line "vé de devenir un"
+	cont "grand dresseur."
+
+	para "Mais pour partir"
+	line "il faut que je"
+	cont "rassemble 10000¥!"
+
+	para "Autant dire que"
+	line "je vais rester"
+	cont "bloqué ici avec"
+	cont "ce fossile sans"
+	cont "valeur longtemps."
+	done
+
+_Lab2TextFossileDome::
+	text "Proposer de"
+	line "lui acheter son"
+	cont "fossile Dôme?"
+	done
+_Lab2TextFossileHelix::
+	text "Proposer de"
+	line "lui acheter son"
+	cont "Nautile?"
+	done
+
+_Lab2TextFossileYes::
+	text "Quoi?! Tu veux"
+	line "vraiment me"
+	cont "l'acheter?"
+
+	para "C'est fantasti-"
+	line "que, merci!"
+	cont "Prends en soin!"
+	
+	para "Il est temps pour"
+	line "moi de partir à"
+	cont "l'aventure!"
+	cont "Pierre, me voilà!"
+	done
+
+_Lab2TextFossileNo::
+	text "Ravie de t'avoir"
+	line "rencontré!"
+	done
+
+_Lab2TextFossileNoMoney::
+	text "C'est gentil de"
+	line "proposer mais ne"
+	cont "me fait pas de"
+	cont "fausse joie si tu"
+	cont "n'as pas l'argent."
+	done
+
+_Lab2TextFossileBagFull::
+	text "Je serais ravi"
+	line "de te le vendre"
+	cont "mais il te faut"
+	cont "de la place!"
 	done
