75770fd6c656de163c7490e5e3a7978362520464
Hunk <mougenotflorent@gmail.com>
2025-02-22 00:42:53 +0100

    feat(#74): reworked casino prizes

--- data/prizes.asm
+++ data/prizes.asm
@@ -13,67 +13,27 @@ NoThanksText:
 	db "Non merci@"
 
 PrizeMenuMon1Entries:
-IF DEF(_ENCRED)
-	db ABRA
-	db CLEFAIRY
-	db NIDORINA
-ELIF DEF(_ENCBLUEJP)
 	db ABRA
 	db PIKACHU
 	db HORSEA
-ELIF DEF(_ENCBLUEGREEN)
-	db ABRA
-	db CLEFAIRY
-	db NIDORINO
-ENDC
 	db "@"
 
 PrizeMenuMon1Cost:
-IF DEF(_ENCRED)
-	coins 180
-	coins 500
-	coins 1200
-ELIF DEF(_ENCBLUEJP)
 	coins 150
-	coins 620
-	coins 1000
-ELIF DEF(_ENCBLUEGREEN)
-	coins 120
 	coins 750
 	coins 1200
-ENDC
 	db "@"
 
 PrizeMenuMon2Entries:
-IF DEF(_ENCRED)
-	db SCYTHER
+	db LARVITAR
 	db DRATINI
 	db PORYGON
-ELIF DEF(_ENCBLUEJP)
-	db CLEFABLE
-	db DRAGONAIR
-	db PORYGON
-ELIF DEF(_ENCBLUEGREEN)
-	db PINSIR
-	db DRATINI
-	db PORYGON
-ENDC
 	db "@"
 
 PrizeMenuMon2Cost:
-IF DEF(_ENCRED)
-	coins 2500
+	coins 3500
 	coins 5200
-	coins 9999
-ELIF DEF(_ENCBLUEJP)
-	coins 2500
-	coins 4600
 	coins 6500
-ELIF DEF(_ENCBLUEGREEN)
-	coins 2800
-	coins 4600
-	coins 6500
-ENDC
 	db "@"
 
 PrizeMenuTMsEntries:
