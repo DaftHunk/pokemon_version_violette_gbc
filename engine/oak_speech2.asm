6bbd2b15be50081166f06067c2cb11ada6cb645c
Hunk <mougenotflorent@gmail.com>
2025-02-22 21:42:53 +0100

    fix(#77): fixed Rival sprite after renaming

--- engine/oak_speech2.asm
+++ engine/oak_speech2.asm
@@ -75,7 +75,7 @@ ChooseRivalName:
 	call ClearScreen
 	call Delay3
 	ld de, Rival1Pic
-	ld b, $13
+	lb bc, Bank(Rival1Pic), $00
 	call IntroDisplayPicCenteredOrUpperRight
 .done
 	ld hl, HisNameIsText
