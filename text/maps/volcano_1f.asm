49d551228643f3cb78470e692df250c4c61c6a68
Hunk <mougenotflorent@gmail.com>
2025-05-03 22:14:12 +0200

    feat(#121): wrote volcano dialogs

--- text/maps/volcano_1f.asm
+++ text/maps/volcano_1f.asm
@@ -1,50 +1,62 @@
 _Volcano1FText0_Battle::
-	text "Tu veux savoir"
-	line "s'il y a des"
-	cont "poissons ici?"
+	text "Attention il fait"
+	line "extrêmement chaud"
+	cont "ici, laisse-moi"
+	cont "t'arroser!"
 	done
 
 _Volcano1FText0_EndBattle::
-	text "Paf!"
+	text "Oh!"
+	line "Tu m'as liquéfié!"
 	prompt
 
 _Volcano1FText0_AfterBattle::
-	text "Y'a comme une"
-	line "absence violente"
-	cont "de poisson!"
+	text "On est ici pour"
+	line "surveiller et"
+	cont "empêcher le vol-"
+	cont "can d'entrer en"
+	cont "éruption!"
 	done
 
 _Volcano1FText1_Battle::
-	text "J'ai eu une bonne"
-	line "prise! Tu veux"
-	cont "tester?"
+	text "On est venu pour"
+	line "s'entrainer!"
+
+	para "Faut pas prendre"
+	line "les volcans à la"
+	cont "légère, ça"
+	cont "demande de la"
+	cont "préparation!"
 	done
 
 _Volcano1FText1_EndBattle::
-	text "Zut!"
-	line "Encore un"
-	cont "Magicarpe!"
+	text "Ah!"
+	line "Toi aussi?"
 	prompt
 
 _Volcano1FText1_AfterBattle::
-	text "Je ne pêche que"
-	line "des Magicarpe!"
+	text "J'ai oublié d'"
+	line "amener mon"
+	cont "#mon eau..."
 	done
 
 _Volcano1FText2_Battle::
-	text "Ah, se faire"
-	line "fouetter la"
-	cont "face par"
-	cont "l'écume..."
-	cont "La douleur... "
-	cont "C'est bon."
+	text "Pousse-toi d'là!"
+	line "Tu vois pas qu'je"
+	cont "bosse?"
 	done
 
 _Volcano1FText2_EndBattle::
 	text "Ayah!"
+	line "Ca brûle!"
 	prompt
 
 _Volcano1FText2_AfterBattle::
-	text "Je vais faire un"
-	line "tour en montagne!"
+	text "Le volcan est in-"
+	line "habituellement"
+	cont "actif depuis"
+	cont "quelques temps,"
+	cont "comme si quelque"
+	cont "chose l'alimen-"
+	cont "tait..."
 	done
--- text/maps/volcano_b1f.asm
+++ text/maps/volcano_b1f.asm
@@ -1,32 +1,40 @@
 _VolcanoB1FText0_Battle::
-	text "Tu veux savoir"
-	line "s'il y a des"
-	cont "poissons ici?"
+	text "Eh gamin! Les"
+	line "vrais gars s'en-"
+	cont "trainent ici!"
 	done
 
 _VolcanoB1FText0_EndBattle::
-	text "Paf!"
+	text "Euh..."
+	line "J'ai peur de"
+	cont "sauter par dessus"
+	cont "la lave..."
 	prompt
 
 _VolcanoB1FText0_AfterBattle::
-	text "Y'a comme une"
-	line "absence violente"
-	cont "de poisson!"
+	text "Va pas l'répéter"
+	line "au chef! Y va"
+	cont "encore me crier"
+	cont "d'su!"
 	done
 
 _VolcanoB1FText1_Battle::
-	text "J'ai eu une bonne"
-	line "prise! Tu veux"
-	cont "tester?"
+	text "Mais qu'est-ce"
+	line "qu'ils foutent"
+	cont "bon dieu?!"
+
+	para "Toi là! Pour qui"
+	line "tu bosses?"
 	done
 
 _VolcanoB1FText1_EndBattle::
-	text "Zut!"
-	line "Encore un"
-	cont "Magicarpe!"
+	text "AHHH"
+	line "EXPLOSION!"
+	cont "J'AI PLUS DE"
+	cont "MUNITIONS!"
 	prompt
 
 _VolcanoB1FText1_AfterBattle::
-	text "Je ne pêche que"
-	line "des Magicarpe!"
+	text "C'était pas ma"
+	line "pas ma guerre!"
 	done
