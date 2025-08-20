4e72729122c2880c466d6e8ec160480600bda03f
Hunk <mougenotflorent@gmail.com>
2025-08-20 21:40:27 +0200

    feat(1.2.2): lab and route 16 west gate improvement

--- data/maps/objects/route16westgate.asm
+++ data/maps/objects/route16westgate.asm
@@ -1,18 +1,18 @@
 Route16WestGateObject:
-	db $a ; border block
+	db $17 ; border block
 
 	db 4 ; warps
-	warp 0, 2,  1, ROUTE_16_WEST
-	warp 0, 3,  2, ROUTE_16_WEST
-	warp 7, 2,  9, ROUTE_16
-	warp 7, 3, 10, ROUTE_16
+	warp 0, 14,  1, ROUTE_16_WEST
+	warp 0, 15,  2, ROUTE_16_WEST
+	warp 7, 14,  9, ROUTE_16
+	warp 7, 15, 10, ROUTE_16
 
 	db 0 ; signs
 
 	db 0 ; objects
 
 	; warp-to
-	warp_to 0, 2, ROUTE_16_WEST_HOUSE_WIDTH
-	warp_to 0, 3, ROUTE_16_WEST_HOUSE_WIDTH
-	warp_to 7, 2, ROUTE_16_WEST_HOUSE_WIDTH
-	warp_to 7, 3, ROUTE_16_WEST_HOUSE_WIDTH
+	warp_to 0, 14, ROUTE_16_WEST_HOUSE_WIDTH
+	warp_to 0, 15, ROUTE_16_WEST_HOUSE_WIDTH
+	warp_to 7, 14, ROUTE_16_WEST_HOUSE_WIDTH
+	warp_to 7, 15, ROUTE_16_WEST_HOUSE_WIDTH
